6c905f17ade7dfff3719c9542be42f39af793b12
Will Larry <will23larry@gmail.com>
2018-03-09 14:09:46 -0500

    Added details for ElasticSearch in job, meeting,  and project models

--- app/models/meeting_post.rb
+++ app/models/meeting_post.rb
@@ -7,3 +7,14 @@ class MeetingPost < ApplicationRecord
     self.tags.map(&:name).join(', ')
   end
 end
+
+# Delete the previous Posts index in Elasticsearch
+MeetingPost.__elasticsearch__.client.indices.delete index: MeetingPost.index_name rescue nil
+
+# Create the new index with the new mapping
+MeetingPost.__elasticsearch__.client.indices.create \
+  index: MeetingPost.index_name,
+  body: { settings: MeetingPost.settings.to_hash, mappings: MeetingPost.mappings.to_hash }
+
+# Index all Post records from the DB to Elasticsearch
+MeetingPost.import
--- app/models/project_post.rb
+++ app/models/project_post.rb
@@ -9,3 +9,14 @@ class ProjectPost < ApplicationRecord
     self.tags.map(&:name).join(', ')
   end
 end
+
+# Delete the previous Posts index in Elasticsearch
+ProjectPost.__elasticsearch__.client.indices.delete index: ProjectPost.index_name rescue nil
+
+# Create the new index with the new mapping
+ProjectPost.__elasticsearch__.client.indices.create \
+  index: ProjectPost.index_name,
+  body: { settings: ProjectPost.settings.to_hash, mappings: ProjectPost.mappings.to_hash }
+
+# Index all Post records from the DB to Elasticsearch
+ProjectPost.import
